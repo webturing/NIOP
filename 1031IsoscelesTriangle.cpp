d4031045077fb6834eb079281e5ede9dbe9bf454
webturing <zj@webturing.com>
2018-06-07 06:23:37 +0800

    打印图形 模拟

--- 1031IsoscelesTriangle.cpp
+++ 1031IsoscelesTriangle.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-void printIsoscelesTriangle(int x);
+void solve(int x);
 
 using namespace std;
 
@@ -8,12 +8,12 @@ int main(int argc, char const *argv[]) {
 
     int x;
     cin >> x;
-    printIsoscelesTriangle(x);
+    solve(x);
 
     return 0;
 }
 
-void printIsoscelesTriangle(int x) {
+void solve(int x) {
     for (int i = 1; i <= x; i++) {
         for (int j = 1; j <= x - i; j++)
             cout << " ";
