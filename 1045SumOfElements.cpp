93f986790fd6b5f3da36eab579564ebc033cf401
zj <zj@ahstu.cc>
2018-06-10 08:38:04 +0800

    利用vector产生二维数组2

--- 1045SumOfElements.cpp
+++ 1045SumOfElements.cpp
@@ -5,12 +5,11 @@ const int ROWS = 4;
 const int COLS = 4;
 
 int main(int argc, char const *argv[]) {
-    ifstream cin("input.txt");
-    vector<vector<int> > A(ROWS); //这个m一定不能少
-    for (int i = 0; i < A.size(); i++) {
-        A[i].resize(COLS);
-        for (auto &e:A[i])cin >> e;
-    }
+    //ifstream cin("input.txt");
+    vector<vector<int> > A(ROWS, vector<int>(COLS));
+    for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < COLS; j++)
+            cin >> A[i][j];
     int sum1 = 0, sum2 = 0;
     for (int i = 0; i < ROWS; i++) {
         sum1 += A[i][i];
